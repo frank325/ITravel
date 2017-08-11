302444d2d2a6c45e58186572f7e50bc25144ca80
Frank_325 <675305383@qq.com>
2017-08-11 16:13:22 +0800

    修改

--- app/src/main/java/lyf/com/example/itravel/ITravelConstant.java
+++ app/src/main/java/lyf/com/example/itravel/ITravelConstant.java
@@ -8,7 +8,6 @@ public class ITravelConstant {
 
     public static int WITTH; //屏幕宽度
 
-//    public final static String URL = "http://192.168.8.106:8989/ITravelWeb/";
-    public final static String URL = "http://192.168.1.111:8989/ITravelWeb/";
+    public final static String URL = "服务器地址";
 
 }
